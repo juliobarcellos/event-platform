dfffe5bde925a60b27ddc312e7c5a3f9196e2401
Julio Barcellos <46910743+juliobarcellos@users.noreply.github.com>
2022-06-24 13:51:32 -0300

    Refatorando para adicionar variáveis locais

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export const client = new ApolloClient ({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oxsc4j1ei401xkc7fzb0g1/master',
-    cache: new InMemoryCache()
-})
+export const client = new ApolloClient({
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
+    cache: new InMemoryCache(),
+});
