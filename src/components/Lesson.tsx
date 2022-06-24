4a9af691f189bc20032fd0b0119fb4ea222f22e7
Julio Barcellos <46910743+juliobarcellos@users.noreply.github.com>
2022-06-23 22:53:45 -0300

    Inserindo rotas e configurando o componente de Video

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
     title: string;
@@ -16,35 +17,34 @@ export function Lesson(props: LessonProps) {
     })
 
     return (
-        <a href="#">
-            <span  className="text-gray-300">
-                {availableDateFormatted.charAt(0).toLocaleUpperCase + availableDateFormatted.slice(1)}
+    <Link to={`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">
+        {availableDateFormatted}
+      </span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo liberado
             </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
 
-            <div className="rounded border-gray-500 p-4 mt-2">
-                <header className="flex items-center justify-between">
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
 
-                    {isLessonAvailable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-                            <CheckCircle size={20} />
-                            Conteúdo Liberado
-                        </span>
-                    ) : (
-                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-                            <Lock size={20} />
-                            Em Breve
-                        </span>
-                    )}
-
-                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-                    </span>
-                </header>
-
-                <strong className="text-gray-200 mt-5 block">
-                    {props.title}
-                </strong>
-            </div>
-        </a>
-    )
+        <strong className="text-gray-200 mt-5 block">
+          {props.title}
+        </strong>
+      </div>
+    </Link>
+  );
 }
